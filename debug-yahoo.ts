942107191b2bde405e71438c0c33d40060b08830
白君彦 <15538894+christian-faith@user.noreply.gitee.com>
2026-04-30 23:50:55 +0800

    chore: Clean up debug files

--- debug-yahoo.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-async function debug() {
-    const stockId = '2330';
-    const url = `https://query1.finance.yahoo.com/v7/finance/quote?symbols=${stockId}.TW`;
-    const res = await axios.get(url, { headers: { 'User-Agent': 'Mozilla/5.0' } });
-    console.log(JSON.stringify(res.data, null, 2));
-}
-debug();
